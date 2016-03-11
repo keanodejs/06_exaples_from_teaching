c697c79ab24ba862e2d5da90d1e86bacc6f8eb7a
Claus Bové <clbo@kea.dk>
2016-03-11 13:57:18 +0100

    comments in server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
-//app.use(express.static('public'));
+// for use when serving static files, like any other webserver does
+// here it is put up to use the public folder, it could be any other folder
+// 
+// app.use(express.static('public')); 
 
-app.set('view engine', 'ejs');
+// set the app to use the "view engine" "ejs"
+// other view engines could also be used if you prefere
+// handlebars, jade etc.
+app.set('view engine', 'ejs'); 
 
+// set up the route for localhost:3000/
 app.get('/', function (req, res) {
 
 	var obj = {
@@ -15,8 +22,15 @@ app.get('/', function (req, res) {
 			return this.name + ' ' + this.title;
 		}
 	}
-
 	res.render('about', obj);
 });
+
+// set up the route for localhost:3000/about
+app.get('/about', function (req, res) {
+	// fill out this function so it is showing some content
+});
+
+// make a contact route
+// // set up the route for localhost:3000/contact
 
 app.listen(3000);
